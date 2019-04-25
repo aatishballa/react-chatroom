5d66e049e965128f70077293bd4eec765bdfb046
aatishballa <aatishballa@gmail.com>
2019-04-24 20:43:42 -0500

    Update to chatkit connection

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import React, { Component } from 'react';
 import RoomList from './components/RoomList';
 import MessageList from './components/MessageList';
 import NewRoomForm from './components/NewRoomForm';
-import SendMessageForm from './components/SendMessageForm'
+import SendMessageForm from './components/SendMessageForm';
 
-import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
-
-import {} from 'module'
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
+import { instanceLocator, tokenUrl } from './config'
+import { } from 'module'
 
 
 class App extends Component {
+
+  componentDidMount() {
+
+    const chatManager = new ChatManager({
+      instanceLocator,
+      userId: 'aballa',
+      tokenProvider: new TokenProvider({
+        url: tokenUrl
+      })
+
+    });
+
+    chatManager.connect()
+      .then(currentUser => {
+            currentUser.subscribeToRoomMultipart({
+                  roomId: '19766999',
+                  hooks:{
+                    onMessage: message =>{
+                      console.log("Received message: ", message);
+                    }
+                  }
+
+            });
+      });
+
+  }
+
   render() {
     return (
       <div className="App">
-      <RoomList />
-      <MessageList />
-      <SendMessageForm />
-      <NewRoomForm />
-  </div>
+        <RoomList />
+        <MessageList />
+        <SendMessageForm />
+        <NewRoomForm />
+      </div>
     );
   }
 }
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/e80d8c2a-020c-4f42-a514-27f9db54131c/token";
+const instanceLocator ="v1:us1:e80d8c2a-020c-4f42-a514-27f9db54131c";
+
+export {tokenUrl,instanceLocator};
